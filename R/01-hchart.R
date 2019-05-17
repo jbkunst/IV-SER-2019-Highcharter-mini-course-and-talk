66dd9024bd0ce787d863f2878f4ad56eb5d5bddc
Joshua <jbkunst@gmail.com>
2019-05-16 23:57:16 -0400

    renamig proyect

--- R/01-hchart.R
+++ R/01-hchart.R
@@ -3,31 +3,75 @@ library(highcharter)
 
 
 # Contexto ----------------------------------------------------------------
+# 
 # plot is a generic (magical) funcion.
+# 
+# What it means "generic"? A generic function will do 
+# an action depending of the "argument" (objetc class)
+# 
+# For example:
+# 
 
-# densidades
+# Densidades
 d <- density(rnorm(1000))
 plot(d)
 
-# survival models
+# Survival models
 library(survival)
-
 data(lung)
-lung <- mutate(lung, sex = ifelse(sex == 1, "Male", "Female"))
-fit <- survfit(Surv(time, status) ~ sex, data = lung) 
+
+fit <- survfit(Surv(time, status) ~ ifelse(sex == 1, "Male", "Female"), data = lung) 
 
 plot(fit)
 
-# time series
-data("AirPassengers")
+# Time series
+data(AirPassengers)
 
 plot(AirPassengers)
 
-# forecast
+# Forecast
 library(forecast)
+
 fit2 <- forecast(AirPassengers)
 
 plot(fit2)
 
-# Principal Components
-plot(princomp(USArrests, cor = TRUE))
+
+# hchart ------------------------------------------------------------------
+# 
+# hchart is a generic function too! :)
+# 
+
+# Densidades
+d <- density(rnorm(1000))
+hchart(d)
+
+# Survival models
+hchart(fit)
+
+# time series
+hchart(AirPassengers)
+
+# forecast
+hchart(fit2)
+
+# Nunerics
+x <- rgamma(3000, 2, 4)
+hchart(x)
+
+# Character
+data(mpg, package = "ggplot2")
+
+hchart(mpg$manufacturer)
+
+# Factor
+data(diamonds, package = "ggplot2")
+
+hchart(diamonds$cut)
+
+# Matrix & correlations
+data(volcano)
+
+hchart(volcano)
+
+hchart(cor(mtcars))
--- R/02-dataframes.R
+++ R/02-dataframes.R
@@ -1,3 +1,32 @@
 # Pacotes -----------------------------------------------------------------
 library(highcharter)
+library(ggplot2)
 library(dplyr)
+
+
+# ggplot2 -----------------------------------------------------------------
+data(diamonds, package = "ggplot2")
+
+set.seed(123)
+diamonds2 <- sample_n(diamonds, 1000)
+
+glimpse(diamonds2)
+
+ggplot(diamonds2) +
+  geom_point(aes(x = carat, y = price, color = cut))
+
+# GEOM / capa
+
+# aestetics 
+
+# see: 
+
+
+# highcharter -------------------------------------------------------------
+
+# Version I
+hchart(diamonds2, "point", hcaes(x = carat, y = price, group = cut))
+
+# Version II
+highchart() %>% 
+  hc_add_series(diamonds2, "point", hcaes(x = carat, y = price, group = cut))
